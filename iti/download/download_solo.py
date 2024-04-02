f0a5cdb99d5d0f91f44586e1eb014ae7040d5385
christoph schirninger <christoph.schirninger@uni-graz.at>
2024-04-02 17:00:58 +0200

    update downloader to specify start and end dates as config

--- iti/download/download_solo.py
+++ iti/download/download_solo.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import shutil
+import time
 from datetime import timedelta, datetime
 from multiprocessing import Pool
 from urllib.request import urlopen
@@ -23,18 +24,20 @@ class SOLODownloader:
 
     def __init__(self, base_path):
         self.base_path = base_path
-        self.wavelengths = ['eui-hri174-image', 'eui-fsi174-image', 'eui-fsi304-image']
-        self.dirs = ['eui-hri174-image', 'eui-fsi174-image', 'eui-fsi304-image']
+        #self.wavelengths = ['eui-hrieuv174-image', 'eui-fsi174-image', 'eui-fsi304-image']
+        self.wavelengths = ['eui-hrieuv174-image']
+        self.dirs = ['eui-hrieuv174-image', 'eui-fsi174-image', 'eui-fsi304-image']
+        #self.dirs = ['eui-hrieuv174-image']
         [os. makedirs(os.path.join(base_path, dir), exist_ok=True) for dir in self.dirs]
 
     def downloadDate(self, date):
         files = []
         try:
             # Download FSI
-            for wl in self.wavelengths[1::]:
-                files += [self.downloadFSI(date, wl)]
-            #for wl in self.wavelengths[0]:
-            #    files += [self.downloadHRI(date, wl)]
+            #for wl in self.wavelengths[1::]:
+            #    files += [self.downloadFSI(date, wl)]
+            for wl in self.wavelengths:
+                files += [self.downloadHRI(date, wl)]
             logging.info('Download complete %s' % date.isoformat())
         except Exception as ex:
             logging.error('Unable to download %s: %s' % (date.isoformat(), str(ex)))
@@ -46,7 +49,7 @@ class SOLODownloader:
         if os.path.exists(file_path):
             return file_path
         #
-        search = Fido.search(a.Time(query_date - timedelta(minutes=10), query_date + timedelta(minutes=10)),
+        search = Fido.search(a.Time(query_date - timedelta(hours=1), query_date + timedelta(hours=1)),
                              a.Instrument('EUI'), a.soar.Product(wl), a.Level(2))
         assert search.file_num > 0, "No data found for %s (%s)" % (query_date.isoformat(), wl)
         search = sorted(search['soar'], key=lambda x: abs(pd.to_datetime(x['Start time']) - query_date).total_seconds())
@@ -74,10 +77,10 @@ class SOLODownloader:
         if os.path.exists(file_path):
             return file_path
         #
-        search = Fido.search(a.Time(query_date - timedelta(minutes=30), query_date + timedelta(minutes=30)),
+        search = Fido.search(a.Time(query_date - timedelta(minutes=1), query_date + timedelta(minutes=1)),
                              a.Instrument('EUI'), a.soar.Product(wl), a.Level(2))
         assert search.file_num > 0, "No data found for %s (%s)" % (query_date.isoformat(), wl)
-        search = sorted(search['soar'], key=lambda x: abs(pd.to_datetime(x['Start Time']) - query_date).total_seconds())
+        search = sorted(search['soar'], key=lambda x: abs(pd.to_datetime(x['Start time']) - query_date).total_seconds())
         #
         for entry in search:
             files = Fido.fetch(entry, path=self.base_path, progress=False)
@@ -104,8 +107,9 @@ if __name__ == '__main__':
 
     #[os.makedirs(os.path.join(base_path, str(c)), exist_ok=True) for c in ['hri_174', 'fsi_174', 'fsi_304']]
     download_util = SOLODownloader(base_path=base_path)
-    start_date = datetime(2023, 3, 7, 0, 0)
+    start_date = datetime(2023, 10, 26, 9, 0)
     end_date = datetime.now()
+    #end_date = datetime(2020, 5, 30, 14, 59)
     #num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
     #month_dates = [start_date + i * relativedelta(months=1) for i in range(num_months)]
     # make weeks_dates
@@ -117,6 +121,6 @@ if __name__ == '__main__':
     #num_hours = (end_date - start_date).days * 24
     #hours_dates = [start_date + i * timedelta(hours=1) for i in range(num_hours)]
 
-    for d in [start_date + i * timedelta(hours=3) for i in
-              range((end_date - start_date) // timedelta(hours=3))]:
+    for d in [start_date + i * timedelta(minutes=1) for i in
+              range((end_date - start_date) // timedelta(minutes=1))]:
         download_util.downloadDate(d)
